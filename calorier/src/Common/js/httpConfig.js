5cc4620feead588a7964dcd5016c236d3efe8d05
92Yan <1789446861@qq.com>
2019-11-25 20:10:45 +0800

    【fix】修复了上次提交只将文件中SignIn改为Regist却没有将文件夹名称改为Regist 的脑残错误

--- calorier/src/Common/js/httpConfig.js
+++ calorier/src/Common/js/httpConfig.js
@@ -10,72 +10,72 @@ import fetch from '@system.fetch'
 import storage from '@system.storage'
 //import router from '@system.router'
 
-const http={}
-const baseUrl='http://192.168.43.28:8080/calorier/apis/v1'
+const http = {}
+const baseUrl = 'http://192.168.43.28:8080/calorier/apis/v1'
 //TODO对token进行赋值
-var token='12345'
-var headers={
- // 'contend-type':'multipart/form-data',
-  'accept':'application/json',
-  'Authorication':token
+var token = '12345'
+var headers = {
+    // 'contend-type':'multipart/form-data',
+    'accept': 'application/json',
+    'Authorication': token
 }
 
 //GET 方法
-http.get=(url)=>{
-    url=baseUrl+url
-   return new Promise((resolve,reject)=>{
-    fetch.fetch ({
-        url:url,
-        cache:'no-cache',
-        headers:headers,
-        method:'GET',
-        mode:'cors',
-        redirect:'follow',
-        referrer:'no-referrer'
-    }).then(response=>{
-        if(response.status===200||response.status===304){
-            if(response.code==1){
-                resolve(response.data)
-            }else{
-                reject(response.message)
+http.get = (url) => {
+    url = baseUrl + url
+    return new Promise((resolve, reject) => {
+        fetch.fetch({
+            url: url,
+            cache: 'no-cache',
+            headers: headers,
+            method: 'GET',
+            mode: 'cors',
+            redirect: 'follow',
+            referrer: 'no-referrer'
+        }).then(response => {
+            if (response.status === 200 || response.status === 304) {
+                if (response.code == 1) {
+                    resolve(response.data)
+                } else {
+                    reject(response.message)
+                }
+            } else {
+                reject(response.code)
             }
-        }else{
-            reject(response.code)
-        }
+        })
     })
-   })
 }
 
 //POST 方法
-http.post=(url,data)=>{
+http.post = (url, data) => {
     console.log(`begin post`)
-    url=baseUrl+url
+    url = baseUrl + url
     console.log(`url is ${url}`)
-    return new Promise((resolve,reject)=>{
-         fetch.fetch({
-             url:url,
-             body:data,
-             cache:'no-cache',
-             credentials:'same-origin',
-             headers:headers,
-             method:'POST',
-             mode:'cors',
-             redirect:'follow',
-             referrer:'no-referrer',
-         }
+    return new Promise((resolve, reject) => {
+        fetch.fetch({
+            url: url,
+            body: data,
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: headers,
+            method: 'POST',
+            mode: 'cors',
+            redirect: 'follow',
+            referrer: 'no-referrer',
+        }
         ).then(
-            response=>{
-                if(response.status===200){
-                    if(response.code===1){
+            response => {
+                if (response.status === 200) {
+                    if (response.code === 1) {
                         resolve(response.data)
-                    }else{
+                    } else {
                         reject(response.message)
                     }
-                }else{
+                } else {
                     reject(response.status)
                 }
             }
-        ).catch(err=>{
+        ).catch(err => {
             console.log(err)
         })
     })
